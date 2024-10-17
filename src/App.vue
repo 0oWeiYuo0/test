7a6a955123df2ee9cf84b32224cb30b33937d0c8
0oWeiYuo0 <s43210712@gmail.com>
2024-10-17 11:37:28 +0800

    清空

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,10 @@
 <script setup>
-import { ref } from "vue";
-
-const num = ref(0)
-
-function warn(message, event) {
-  // 這裡可以訪問原生事件
-  if (event) {
-    event.preventDefault()
-  }
-  alert(message)
-}
-
-const handleKeyup = (event) => {
-  if (event.altKey && event.key === 'Enter') {
-    clear();
-  }
-};
-
-const clear = () => {
-  console.log('Clearing the input!');
-};
 
 </script>
 
 <template>
   <div>
-<<<<<<< HEAD
-    <input type="text" @keyup="handleKeyup">
-=======
-    <!-- 單擊事件將停止傳遞 -->
-    <a @click.stop="doThis"></a>
 
-    <!-- 提交事件將不再重新加載頁面 -->
-    <form @submit.prevent="onSubmit"></form>
-
-    <!-- 修飾語可以使用鏈式書寫 -->
-    <a @click.stop.prevent="doThat"></a>
-
-    <!-- 也可以只有修飾符 -->
-    <form @submit.prevent></form>
-
-    <!-- 僅當 event.target 是元素本身時才會觸發事件處理器 -->
-    <!-- 例如：事件處理器不來自子元素 -->
-    <div @click.self="doThat">...</div>
->>>>>>> parent of 814a781 (測試)
   </div>
 </template>
 
