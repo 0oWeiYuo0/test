5a1c83ae9fed8310aad73fa17f359c6970ead38e
0oWeiYuo0 <s43210712@gmail.com>
2024-10-17 17:24:58 +0800

    動態渲染選擇器

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+
+
+const selected = ref('請選擇')
+
+const fruits = ref([
+  { text: '蘋果', value: 'apple' },
+  { text: '香蕉', value: 'banana' },
+  { text: '西瓜', value: 'watermelon' }
+])
+
+
 
 </script>
 
 <template>
   <div>
+    請選擇
+    <select v-model="selected">
+      <option v-for="(option) in fruits" :value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
 
+
+    <div>被選擇的是：{{ selected }}</div>
   </div>
 </template>
 
