aaf68358d2c3ff7ca66b1419c3a62a204900297e
0oWeiYuo0 <s43210712@gmail.com>
2024-10-17 10:33:00 +0800

    git add

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+
+const num = ref(0)
+
+function warn(message, event) {
+  // 這裡可以訪問原生事件
+  if (event) {
+    event.preventDefault()
+  }
+  alert(message)
+}
+
+</script>
+
+<template>
+  <div>
+    <!-- 單擊事件將停止傳遞 -->
+    <a @click.stop="doThis"></a>
+
+    <!-- 提交事件將不再重新加載頁面 -->
+    <form @submit.prevent="onSubmit"></form>
+
+    <!-- 修飾語可以使用鏈式書寫 -->
+    <a @click.stop.prevent="doThat"></a>
+
+    <!-- 也可以只有修飾符 -->
+    <form @submit.prevent></form>
+
+    <!-- 僅當 event.target 是元素本身時才會觸發事件處理器 -->
+    <!-- 例如：事件處理器不來自子元素 -->
+    <div @click.self="doThat">...</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/TodoItem.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['title'])
+defineEmits(['remove'])
+
+</script>
+
+<template>
+  <div>
+    <li>
+      {{ title }}
+      <button @click="$emit('remove')">刪除</button>
+    </li>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
